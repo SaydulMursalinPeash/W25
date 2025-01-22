6906aaa95855cd98fd13d16aa042392370a868d0
SaydulMursalinPeash <saydulpeash428@gmail.com>
2025-01-22 14:42:19 +0600

    launch

--- launch/bridge.launch.py
+++ launch/bridge.launch.py
@@ -1,33 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-    bridge_name = LaunchConfiguration('bridge_name')
-    config_file = LaunchConfiguration('config_file')
-    
-    declare_bridge_name_cmd = DeclareLaunchArgument(
-        'bridge_name',
-        description='Name of the ros_gz_bridge node'
+    config_file_path = os.path.join(
+        get_package_share_directory('W25'), 'launch', 'bridge_config.yaml'
     )
-    
-    declare_config_file_cmd = DeclareLaunchArgument(
-        'config_file',
-        description='Path to the config file'
-    )
-    
-    bridge_node = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        name=bridge_name,
-        parameters=[config_file],
-        output='screen'
-    )
-    
-    ld = LaunchDescription()
-    ld.add_action(declare_bridge_name_cmd)
-    ld.add_action(declare_config_file_cmd)
-    ld.add_action(bridge_node)
-    
-    return ld
+
+    return LaunchDescription([
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='parameter_bridge',
+            output='screen',
+            parameters=[
+                {'config_file': config_file_path}
+            ]
+        )
+    ])
