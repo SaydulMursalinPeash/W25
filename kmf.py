ced65021d0dbb14d1e22a803138308e4b89cd5a9
SaydulMursalinPeash <saydulpeash428@gmail.com>
2025-01-23 18:12:15 +0600

    xfjdh

--- kmf.py
+++ kmf.py
@@ -20,7 +20,11 @@ class KalmanFilter(Node):
         self.last_angular_z_ = 0.0
         self.motion_ = 0.0
         self.kalman_odom = Odometry()
-
+        self.motion_variance_ = 4.0
+        self.measurement_variance_ = 0.5
+    def measurement_update(self):
+        self.mean_ = (self.measurement_variance_ * self.mean_ + self.variance_ * self.imu_angular_z_) / (self.variance_ + self.measurement_variance_)
+        self.variance_ = (self.variance_ * self.measurement_variance_) / (self.variance_ + self.measurement_variance_)
     def odom_callback(self,msg):
         self.kalman_odom = msg
         if self.is_first_odom_:
@@ -29,7 +33,7 @@ class KalmanFilter(Node):
             self.is_first_odom_ = False
             return 
         self.StatePrediction()
-        self.StateUpdate
+        self.StateUpdate()
 
     def imu_callback(self,msg):
         self.imu_angular_z_ = msg.angular_velocity.z
