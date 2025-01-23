d21c1e7c486d08849364de4fa29ced7e509434e1
SaydulMursalinPeash <saydulpeash428@gmail.com>
2025-01-23 17:37:12 +0600

    srgt

---
+++ kmf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3 
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg  import Imu
+
+
+
+class KalmanFilter(Node):
+    def __init__(self):
+        super().__init__('kalman_filter')
+        self.get_logger().info('Kalman Filter Node has been started')
+        self.odom_subscriber_ = self.create_subscription(Odometry,"/w25/odom",self.odom_callback,10)
+        self.imu_subscriber_ = self.create_subscription(Imu,"/w25/imu",self.imu_callback,10)
+        self.odom_publisher_ = self.create_publisher(Odometry,"/w25/odom_filtered",10)
+        self.mean_ = 0.0
+        self.variance_ = 1000
+        self.imu_angular_z_ = 0.0
+        self.is_first_odom_ = True
+        self.last_angular_z_ = 0.0
+        self.motion_ = 0.0
+        self.kalman_odom = Odometry()
+
+    def odom_callback(self,msg):
+        self.kalman_odom = msg
+        if self.is_first_odom_:
+            self.mean_ = msg.twist.twist.angular.z
+            self.last_angular_z_ = msg.twist.twist.angular.z
+            self.is_first_odom_ = False
+            return 
+        self.StatePrediction()
+        self.StateUpdate
+
+    def imu_callback(self,msg):
+        self.imu_angular_z_ = msg.angular_velocity.z
+
