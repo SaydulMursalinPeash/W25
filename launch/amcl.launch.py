fffb71c3d1702400c99806b3491f05724608eb89
SaydulMursalinPeash <saydulpeash428@gmail.com>
2025-01-27 17:56:07 +0600

    Add AMCL configuration and launch files for map integration

---
+++ launch/amcl.launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    map_conf = os.path.join(get_package_share_directory('W25'),'map','wirehouse_map.yaml')
+    amcl_conf = os.path.join(get_package_share_directory('W25'),'config','amcl.yaml')
+    return LaunchDescription(
+        [
+            Node(
+                package='nav2_map_server',
+                executable='map_server',
+                name='map_server',
+                output='screen',
+                parameters=[
+                    {'yaml_filename': map_conf}
+                ]
+            ),
+            Node(
+                package='nav2_amcl',
+                executable='amcl',
+                output='screen',
+                name='amcl',
+                parameters=[
+                    amcl_conf
+                ]
+            ),
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom'],
+            )
+        ]
+    )
+
+
+
+######    ros2 lifecycle set /map_server configure
+######    ros2 lifecycle set /map_server activate
+######    ros2 lifecycle set /amcl configure
+######    ros2 lifecycle set /amcl activate
