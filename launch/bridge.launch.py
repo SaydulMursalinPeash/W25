a688e1681e0145a5a72b42e3f4bf76ad73380c2b
SaydulMursalinPeash <saydulpeash428@gmail.com>
2025-01-31 01:55:22 +0600

    Add configuration files for tugbot map and lidar filtering

--- launch/bridge.launch.py
+++ launch/bridge.launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     config_file_path = os.path.join(
         get_package_share_directory('W25'), 'config', 'bridge_config.yaml'
     )
-    ekf_config = '/home/peash/mobile_ws/src/W25/config/ekf.yaml'#os.path.join(get_package_share_directory('W25'), 'config', 'ekf.yaml')
+    # ekf_config = '/home/peash/mobile_ws/src/W25/config/ekf.yaml'#os.path.join(get_package_share_directory('W25'), 'config', 'ekf.yaml')
 
     return LaunchDescription([
         Node(
@@ -19,19 +19,19 @@ def generate_launch_description():
                 {'config_file': config_file_path}
             ]
         ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='back_lidar_tf',
-            arguments=['-0.2075', '0.0', '0.205', '0.0', '0.0', '1.0', '0.0', 'base_link', 'tugbot/scan_back/scan_back']
-        ),
-        # Static transform for front LIDAR
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='front_lidar_tf',
-            arguments=['0.221', '0.0', '0.1404', '0.0', '0.0', '0.0', 'base_link', 'tugbot/scan_front/scan_front']
-        ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='back_lidar_tf',
+        #     arguments=['-0.2075', '0.0', '0.205', '0.0', '0.0', '1.0', '0.0', 'base_link', 'tugbot/scan_back/scan_back']
+        # ),
+        # # Static transform for front LIDAR
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='front_lidar_tf',
+        #     arguments=['0.221', '0.0', '0.1404', '0.0', '0.0', '0.0', 'base_link', 'tugbot/scan_front/scan_front']
+        # ),
         # Static transform for front camera
         Node(
             package='tf2_ros',
@@ -40,24 +40,24 @@ def generate_launch_description():
             arguments=['0.0553', '0.0', '0.4323', '0.0', '0.0', '0.0', 'base_link', 'tugbot/camera_front/camera_front']
         ),
         # Static transform for back camera
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='back_camera_tf',
-            arguments=['-0.241', '0.0', '0.2303', '0.0', '0.0', '3.141592654', 'base_link', 'tugbot/camera_back/camera_back']
-        ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='back_camera_tf',
+        #     arguments=['-0.241', '0.0', '0.2303', '0.0', '0.0', '3.141592654', 'base_link', 'tugbot/camera_back/camera_back']
+        # ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='lidar_omni_tf',
-            arguments=['0.221', '0.0', '0.1404', '0.0', '0.0', '0.0', 'base_link', 'tugbot/scan_omni/scan_omni']
+            arguments=['0.0', '0.0', '0.4523', '0.0', '0.0', '0.0', 'base_link', 'tugbot/scan_omni/scan_omni']
         ),
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='map_to_odom_tf',
-        #     arguments=['0.0','0.0','0.0', '0.0', '0.0', '0.0', 'map', 'odom']
-        # )
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='map_to_odom_tf',
+            arguments=['0.0','0.0','0.0', '0.0', '0.0', '0.0', 'map', 'odom']
+        )
         # Node(
         #     package='robot_localization',
         #     executable='ekf_node',
